fb262542e3b5080859c37e2bfa5abce159e85ea2
ctyeh <ctyeh@post.harvard.edu>
2013-10-24 15:08:18 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kp::Application.config.secret_token = '7ca6fe3d066687f447c2804b0faf5a87099f1cd7a7a8254e3d71a00401dbe898b070669153596fffc5bfd006fedae845acc5321042263b2bba785bd4ef075d56'
